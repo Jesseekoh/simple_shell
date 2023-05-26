15b936cba50ab3b09c0261f3f517aae87ef1a06a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-05-26 16:55:19 +0100

    Fix betty error: getline.c

--- getline.c
+++ getline.c
@@ -2,6 +2,12 @@
 
 #define BUF_SIZE 1024
 
+/**
+ * _getline - gets input from user and stores it in a variable
+ * @lineptr: variable to store the user input
+ * @n: initial size
+ * Return: size_t
+ */
 size_t _getline(char **lineptr, size_t *n)
 {
 	size_t new_size = 0;
@@ -35,25 +41,35 @@ size_t _getline(char **lineptr, size_t *n)
 				return (-1);
 		}
 	}
-
 }
 
+/**
+ * _getchar - our implementation of C standard library getchar
+ * Return: int
+ */
 int _getchar(void)
 {
 	char buffer[BUF_SIZE];
 	static ssize_t rd = 0;
 	static int bu_p = 0;
 
-       if (bu_p >= rd)
-       {
+	if (bu_p >= rd)
+	{
 		rd = read(STDIN_FILENO, buffer, BUF_SIZE);
 		bu_p = 0;
 		if (rd <= 0)
 			return (EOF);
-       }
+	}
 
 	return ((int)buffer[bu_p++]);
 }
+/**
+ * _realloc - our implementation of C standard library realloc
+ * @ptr: pointer
+ * @old_size: initial size
+ * @new_size: new size
+ * Return: void
+ */
 
 void *_realloc(void *ptr, size_t old_size, size_t new_size)
 {
@@ -70,7 +86,7 @@ void *_realloc(void *ptr, size_t old_size, size_t new_size)
 	if (new_size == 0 && ptr_cpy != NULL)
 	{
 		free(ptr_cpy);
-		return(ptr);
+		return (ptr);
 	}
 	ptr_cpy = ptr;
 	new_mem = malloc(new_size);
@@ -86,6 +102,13 @@ void *_realloc(void *ptr, size_t old_size, size_t new_size)
 	return (new_mem);
 }
 
+/**
+ * mem_cpy - copy mem
+ * @dest: pointer
+ * @src: pointer
+ * @n: size_t
+ * Return: void
+ */
 void *mem_cpy(void *dest, void *src, size_t n)
 {
 	size_t i;
